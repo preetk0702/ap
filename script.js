8901a8db8b6ae2a397b4e590e9e8d106a4f710ca
preetk0702 <71415457+preetk0702@users.noreply.github.com>
2026-01-15 02:39:40 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.btn').forEach(btn => {
-  btn.addEventListener('mousemove', e => {
-    const rect = btn.getBoundingClientRect()
-    btn.style.transform = `translate(${(e.clientX - rect.left - rect.width/2)/10}px, ${(e.clientY - rect.top - rect.height/2)/10}px)`
-  })
-  btn.addEventListener('mouseleave', () => {
-    btn.style.transform = 'translate(0,0)'
-  })
-})
